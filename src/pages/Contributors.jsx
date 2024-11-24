100bc3796d59989d550d9e19c2c544527e2e385d
mitchford20 <mitchrford01@gmail.com>
2024-11-23 22:02:39 -0500

    hopefully fix pages issue

--- src/pages/Contributors.jsx
+++ src/pages/Contributors.jsx
@@ -19,7 +19,7 @@ function Contributors() {
     { 
       name: 'Muneeb Ali Asif', 
       role: 'Frontend Developer', 
-      image: '/placeholder.svg?height=150&width=150&text=CW',
+      image: '/muneeb.png?height=150&width=150&text=CW',
       bio: 'Carols keen eye for design and user experience has been instrumental in creating WanderSyncs intuitive and visually appealing interface.'
     },
     { 
@@ -30,13 +30,13 @@ function Contributors() {
     },
     { 
       name: 'Mitchell Ford', 
-      role: 'Fullstack Developer', 
+      role: 'Fullstack Developer/ Website Designer :)', 
       image: '/mitch.png',
       bio: 'Evas meticulous approach to quality assurance has ensured that WanderSync maintains high standards of reliability and performance.'
     },
     { 
       name: 'Rayan Castilla', 
-      role: 'Frontend Developer', 
+      role: 'Frontend Developer/ Website Designer :)', 
       image: '/rayan.png',
       bio: 'Franks skills in DevOps have streamlined WanderSyncs development pipeline and improved overall system stability.'
     },
